ebb30f0b727148350162c27989dcec23b7fe9936
sylee <seungyeoni.dev@gmail.com>
2025-05-26 17:15:26 +0900

    [Dev] 빌드설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/seungyeonee.github.io/",
   plugins: [react()],
   build: {
     outDir: "dist", // 빌드 결과물이 저장될 폴더
